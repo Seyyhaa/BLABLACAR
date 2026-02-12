c85ac90d182c97ba1e06f22cd5fdad075aa86af7
Seyha Kruy <seyhakruy168168@gmail.com>
2026-02-12 22:57:41 +0700

    BLA - 003: Implement Form

--- lib/ui/screens/ride_pref/widgets/ride_pref_form.dart
+++ lib/ui/screens/ride_pref/widgets/ride_pref_form.dart
@@ -1,8 +1,13 @@
+import 'package:blabla/ui/screens/location_picker/location_picker_screen.dart';
+import 'package:blabla/ui/theme/theme.dart';
+import 'package:blabla/ui/widgets/actions/bla_button.dart';
+import 'package:blabla/ui/widgets/display/bla_divider.dart';
+import 'package:blabla/utils/date_time_utils.dart';
 import 'package:flutter/material.dart';
- 
-import '../../../../model/ride/locations.dart';
-import '../../../../model/ride_pref/ride_pref.dart';
- 
+
+import '../../../../models/ride/locations.dart';
+import '../../../../models/ride_pref/ride_pref.dart';
+
 ///
 /// A Ride Preference From is a view to select:
 ///   - A depcarture location
@@ -28,8 +33,6 @@ class _RidePrefFormState extends State<RidePrefForm> {
   Location? arrival;
   late int requestedSeats;
 
-
-
   // ----------------------------------
   // Initialize the Form attributes
   // ----------------------------------
@@ -37,18 +40,66 @@ class _RidePrefFormState extends State<RidePrefForm> {
   @override
   void initState() {
     super.initState();
-    // TODO 
+    // TODO
+    departure = widget.initRidePref?.departure;
+    departureDate = widget.initRidePref?.departureDate ?? DateTime.now();
+    arrival = widget.initRidePref?.arrival;
+    requestedSeats = widget.initRidePref?.requestedSeats ?? 1;
   }
 
   // ----------------------------------
   // Handle events
   // ----------------------------------
- 
+  _onSwapLocation() {
+    setState(() {
+      //use for swap the Location
+      final temp = departure;
+      departure = arrival;
+      arrival = temp;
+    });
+  }
+
+  _selectedLocation({required bool isDeparture}) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationPickerScreen(
+          onSelected: (Location selectedLocation) {
+            setState(() {
+              if (isDeparture) {
+                departure = selectedLocation;
+              } else {
+                arrival = selectedLocation;
+              }
+            });
+          },
+        ),
+      ),
+    );
+  }
 
   // ----------------------------------
   // Compute the widgets rendering
   // ----------------------------------
-  
+  Widget _buildChooseTile({
+    required IconData icon,
+    required String label,
+    bool isPlaceholder = false,
+    Widget? trailing,
+    required VoidCallback onTap,
+  }) {
+    return ListTile(
+      onTap: onTap,
+      leading: Icon(icon, color: BlaColors.neutral),
+      title: Text(
+        label,
+        style: BlaTextStyles.body.copyWith(
+          color: isPlaceholder ? BlaColors.textLight : BlaColors.textNormal,
+        ),
+      ),
+      trailing: trailing,
+    );
+  }
 
   // ----------------------------------
   // Build the widgets
@@ -56,10 +107,41 @@ class _RidePrefFormState extends State<RidePrefForm> {
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [ 
- 
-        ]);
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        _buildChooseTile(
+          label: departure?.name ?? "Leaving from",
+          icon: Icons.radio_button_unchecked,
+          isPlaceholder: departure == null,
+          onTap: () => _selectedLocation(isDeparture: true),
+          trailing: IconButton(
+            icon: Icon(Icons.swap_vert, color: BlaColors.primary),
+            onPressed: _onSwapLocation,
+          ),
+        ),
+        BlaDivider(),
+        _buildChooseTile(
+          label: arrival?.name ?? "Going to",
+          icon: Icons.radio_button_unchecked,
+          isPlaceholder: arrival == null,
+          onTap: () => _selectedLocation(isDeparture: false),
+        ),
+        BlaDivider(),
+        _buildChooseTile(
+          icon: Icons.date_range,
+          label: DateTimeUtils.formatDateTime(departureDate),
+          onTap: () {},
+        ),
+        BlaDivider(),
+        _buildChooseTile(
+          icon: Icons.person,
+          label: requestedSeats.toString(),
+          onTap: () {},
+        ),
+        BlaDivider(),
+        BlaButton(onPressed: () {}, label: "Search"),
+      ],
+    );
   }
 }
