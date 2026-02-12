a06bcccde4a9503593015a5732f9eb5ba168fce4
Seyha Kruy <seyhakruy168168@gmail.com>
2026-02-12 16:39:59 +0700

    REFACTOR: Remove Duplicated Files

--- test/test_rides.dart
+++ test/test_rides.dart
@@ -1,12 +1,12 @@
-import 'package:blabla/model/ride/ride.dart';
+import 'package:blabla/models/ride/ride.dart';
 
-import 'package:blabla/model/ride/locations.dart';
+import 'package:blabla/models/ride/locations.dart';
 import 'package:blabla/services/rides_service.dart';
 
 void main() {
   Location dijon = Location(country: Country.france, name: "Dijon");
 
-  List<Ride> filteredRide = RidesService.filter(requestedSeats: 2, departureLocation: dijon);
+  List<Ride> filteredRide = RidesService.filterBy(seatRequested: 2, departure: dijon);
 
   for (Ride ride in filteredRide) {
     print(ride);
